d9ca12ce43a20af68fae863d4f9c049f1a9a9db0
Goloschapov Dmitry <goink228@gmail.com>
2022-11-08 22:21:13 +0300

    lab5

--- src/main/java/ru/desyatova/trsis/lab2/config/ControllersConfig.java
+++ src/main/java/ru/desyatova/trsis/lab2/config/ControllersConfig.java
@@ -1,8 +1,13 @@
 package ru.desyatova.trsis.lab2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class ControllersConfig implements WebMvcConfigurer  {
@@ -17,4 +22,20 @@ public class ControllersConfig implements WebMvcConfigurer  {
 
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
 }
