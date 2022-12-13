d1aea9b721406c827598203dc7c30b30b816931a
Goloschapov Dmitry <goink228@gmail.com>
2022-12-13 22:50:51 +0300

    lab7

--- src/main/java/ru/desyatova/trsis/lab2/exceptions/DuplicateRentException.java
+++
@@ -1,8 +0,0 @@
-package ru.desyatova.trsis.lab2.exceptions;
-
-public class DuplicateRentException extends RuntimeException{
-    public static final String ALREADY_EXISTS = "Аренда с id %d уже существует";
-    public DuplicateRentException(String message) {
-        super(message);
-    }
-}
